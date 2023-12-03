5b063cb587104fc1f08ef2bb7624ac5432816b8b
grenition <arseniy.mikhep@gmail.com>
2023-12-04 00:37:27 +0300

    Health Added

--- Assets/Scripts/Entities/Health.cs
+++ Assets/Scripts/Entities/Health.cs
@@ -6,17 +6,45 @@ using System;
 
 public class Health : MonoBehaviour
 {
+    public float HealthPointsProportion
+    {
+        get => HealthPoints / maxHealthPoints;
+    }
     public float HealthPoints
     {
         get => currentHealth;
         set
         {
-
+            value = Math.Clamp(value, 0f, maxHealthPoints);
+            if (value == currentHealth)
+                return;
+            currentHealth = Math.Clamp(value, 0f, maxHealthPoints);
+            OnHealthChanged?.Invoke();
+            if (currentHealth == 0f)
+                OnHealthEnded?.Invoke();
+            if (currentHealth == maxHealthPoints)
+                OnRegenerate?.Invoke();
         }
     }
 
-    [SerializeField] private float currentHealth;
+    [SerializeField] private float currentHealth = 100f;
+    [SerializeField] private float maxHealthPoints = 100f;
+
 
     public UnityAction OnHealthChanged;
     public UnityAction OnHealthEnded;
+    public UnityAction OnRegenerate;
+
+    public void Regenerate()
+    {
+        HealthPoints = maxHealthPoints;
+    }
+    public void Die()
+    {
+        HealthPoints = 0f;
+    }
+    public virtual void Damage(float damage)
+    {
+        HealthPoints -= damage;
+    }
 }
